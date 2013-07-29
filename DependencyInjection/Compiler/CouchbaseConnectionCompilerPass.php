6daefc00c5680c898367e92263ebcc435d529e19
antoine <antoine.reyt@gmail.com>
2013-07-29 20:40:02 +0200

    add doc

--- DependencyInjection/Compiler/CouchbaseConnectionCompilerPass.php
+++ DependencyInjection/Compiler/CouchbaseConnectionCompilerPass.php
@@ -6,6 +6,9 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
+/**
+ * Create dynamically the couchbase.connection.<connectionName> services using the configuration.
+ */
 class CouchbaseConnectionCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
@@ -16,11 +19,19 @@ class CouchbaseConnectionCompilerPass implements CompilerPassInterface
         $container->addDefinitions($connectionServicesDefinitions);
     }
 
+    /**
+     * Get the connections services definitions from the configuration.
+     *
+     * @param  array $connectionsConfigurations : all the connections parameters
+     *
+     * @return array of Definiton
+     */
     public function getDefinitions($connectionsConfigurations)
     {
         $definitions = array();
 
         foreach ($connectionsConfigurations as $name => $params) {
+            // Build arguments
             $args = array(
                 sprintf('%s:%s', $params['host'], $params['port']),
                 $params['username'],
@@ -28,8 +39,11 @@ class CouchbaseConnectionCompilerPass implements CompilerPassInterface
                 $params['bucket'],
             );
 
+            // Build definition
             $definition = new Definition('Couchbase', $args);
             $id = sprintf('couchbase.connection.%s', $name);
+
+            // Append definitions array
             $definitions[$id] = $definition;
         }
 
