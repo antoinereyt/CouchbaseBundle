11553e3e4aa886266ee3e37328c58682d150f423
antoine <areyt@powerspace.com>
2013-07-31 18:29:06 +0200

    fix Connection class according to it's ConnextionInterface

--- Connexion/Connexion.php
+++ Connexion/Connexion.php
@@ -24,4 +24,37 @@ class Connexion implements ConnexionInterface
     {
         $this->couchbase = $couchbase;
     }
+
+    /**
+     * Get a Document by it's key.
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->couchbase->get($key);
+    }
+
+    /**
+     * Push a Document to Couchbase.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        $this->couchbase->set($key, $value);
+    }
+
+    /**
+     * Delete a Document from Couchbase.
+     *
+     * @param string $key
+     */
+    public function delete($key)
+    {
+        $this->couchbase->delete($key);
+    }
 }
