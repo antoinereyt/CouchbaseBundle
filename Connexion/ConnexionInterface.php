913af3d5a3f64fc6f3da12ae90959c8c5d15c85c
antoine <areyt@powerspace.com>
2013-07-31 16:22:15 +0200

    add connexionInterface

--- Connexion/ConnexionInterface.php
+++ Connexion/ConnexionInterface.php
@@ -4,5 +4,9 @@ namespace Toiine\Bundle\CouchbaseBundle\Connexion;
 
 interface ConnexionInterface
 {
+    /**
+     * Connexion to a Couchbase bucket
+     * @var Couchbase
+     */
     protected $couchbase;
 }
