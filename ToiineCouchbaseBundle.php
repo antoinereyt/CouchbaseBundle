ae627c24645655261acfc86132e1dd002d0d0bad
antoine <areyt@powerspace.com>
2013-07-31 12:08:10 +0200

    update Bundle class to use the new 3 compiler passes

--- ToiineCouchbaseBundle.php
+++ ToiineCouchbaseBundle.php
@@ -5,7 +5,9 @@ namespace Toiine\Bundle\CouchbaseBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Toiine\Bundle\CouchbaseBundle\DependencyInjection\Compiler\CouchbaseConnectionCompilerPass;
+use Toiine\Bundle\CouchbaseBundle\DependencyInjection\Compiler\ConnectionCompilerPass;
+use Toiine\Bundle\CouchbaseBundle\DependencyInjection\Compiler\DocumentManagerCompilerPass;
+use Toiine\Bundle\CouchbaseBundle\DependencyInjection\Compiler\RepositoryCompilerPass;
 
 class ToiineCouchbaseBundle extends Bundle
 {
@@ -13,6 +15,8 @@ class ToiineCouchbaseBundle extends Bundle
     {
         parent::build($container);
 
-        $container->addCompilerPass(new CouchbaseConnectionCompilerPass());
+        $container->addCompilerPass(new ConnectionCompilerPass());
+        $container->addCompilerPass(new DocumentManagerCompilerPass());
+        $container->addCompilerPass(new RepositoryCompilerPass());
     }
 }
