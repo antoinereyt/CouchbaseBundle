ef1cc47d54a8431fa421eeb385c0efd3b16c54ab
antoine <areyt@powerspace.com>
2013-07-31 16:44:21 +0200

    fix ConnectionInterface

--- Connexion/ConnexionInterface.php
+++ Connexion/ConnexionInterface.php
@@ -5,8 +5,26 @@ namespace Toiine\Bundle\CouchbaseBundle\Connexion;
 interface ConnexionInterface
 {
     /**
-     * Connexion to a Couchbase bucket
-     * @var Couchbase
+     * Get a Document by it's key.
+     *
+     * @param  string $key
+     *
+     * @return mixed
      */
-    protected $couchbase;
+    public function get($key);
+
+    /**
+     * Push a Document to Couchbase.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value);
+
+    /**
+     * Delete a Document from Couchbase.
+     *
+     * @param string $key
+     */
+    public function delete($key);
 }
