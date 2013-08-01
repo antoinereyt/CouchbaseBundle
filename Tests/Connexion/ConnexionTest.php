ea1ad2426b5217594ac0b04cb9611beb2b1c0e14
antoine <areyt@powerspace.com>
2013-08-01 11:10:34 +0200

    make connexion testable without having libcouchbase installed

--- Tests/Connexion/ConnexionTest.php
+++ Tests/Connexion/ConnexionTest.php
@@ -2,9 +2,6 @@
 
 namespace Toiine\Bundle\CouchbaseBundle\Tests\Connexion;
 
-require_once 'Phake.php';
-use \Phake;
-
 use Toiine\Bundle\CouchbaseBundle\Connexion\Connexion;
 
 class ConnexionTest extends \PHPUnit_Framework_TestCase
@@ -12,32 +9,50 @@ class ConnexionTest extends \PHPUnit_Framework_TestCase
     /** @covers Toiine\Bundle\CouchbaseBundle\Connexion\Connexion::__construct */
     public function setUp()
     {
-        $this->couchbase = Phake::mock('Couchbase');
-        Phake::when($this->couchbase)->get('key')->thenReturn(array('value'));
-        $this->connexion = new Connexion($this->couchbase);
+        $this->couchbase = $this->getMockBuilder('nonexistant')
+            ->setMockClassName('Couchbase')
+            ->setMethods(array('get', 'set', 'delete'))
+            ->getMock();
+
+        $connexion = new Connexion($this->couchbase);
     }
 
     /** @covers Toiine\Bundle\CouchbaseBundle\Connexion\Connexion::get */
     public function testSet()
     {
-        $this->connexion->get('key');
+        $connexion = new Connexion($this->couchbase);
 
-        Phake::verify($this->couchbase)->get('key');
+        $this->couchbase
+            ->expects($this->once())
+            ->method('get')
+        ;
+
+        $connexion->get('');
     }
 
     /** @covers Toiine\Bundle\CouchbaseBundle\Connexion\Connexion::set */
     public function testGet()
     {
-        $this->connexion->set('key', array('value'));
+        $connexion = new Connexion($this->couchbase);
 
-        Phake::verify($this->couchbase)->set('key', array('value'));
+        $this->couchbase
+            ->expects($this->once())
+            ->method('set')
+        ;
+
+        $connexion->set('key', array('value'));
     }
 
     /** @covers Toiine\Bundle\CouchbaseBundle\Connexion\Connexion::delete */
     public function testDelete()
     {
-        $this->connexion->delete('key');
+        $connexion = new Connexion($this->couchbase);
 
-        Phake::verify($this->couchbase)->delete('key');
+        $this->couchbase
+            ->expects($this->once())
+            ->method('delete')
+        ;
+
+        $connexion->delete('key');
     }
 }
